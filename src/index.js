7f240d2ee4694c85d365fefe289923d3cfd003b2
serkanekinci <serkan.eekinci@gmail.com>
2017-01-23 10:38:32 +0300

    init

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ render(
         <Grid>
             <RApplication language={tr}>
                 <h1>Todo Application</h1>
-                <h3>qfdqfqvsdvdsvdsfddaassdvsdeqf</h3>
+                <h3>qfdqfqvsdcscdvdsvdsfddaassdvsdeqf</h3>
                 <Todo />
             </RApplication>
         </Grid>
